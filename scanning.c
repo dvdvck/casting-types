06fbc871eb8fd4dc090c76e75449a648aff50e21
Dummy Hoe <dvdvck@gmail.com>
2014-03-20 19:18:56 -0600

    Generacion del tamaño del array de forma dinamica
    A traves del primer argumento se recibe cuantos numeros se generan
    
    Esta implementacion es parcial ya que el llenado de la informacion
    esta fija

--- scanning.c
+++ scanning.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main ( void ){
 
-	int arrint[256];
-	char arrcha[256][12];
-	int i;
-	int numelem=0;
+int main (int argc, char *argv[]){
+
+	unsigned short *input;
+	char **output;
+	int numelem, i;
 	
+	if(argc < 2){
+		numelem = 6;
+	}else{
+		numelem = strtol(argv[1], NULL, 0);
+	}
+
+	input = malloc(sizeof(short) * numelem);
+	output = malloc(sizeof(char **) * numelem);
+
+	for(i = 0; i < numelem; i++){
+		*(output+i) = malloc(sizeof(char *) * 12);
+	}
+
 	//Llenado manual... no se de donde vayan a salir estos datos
-	arrint[0]=4;
-	arrint[1]=8;
-	arrint[2]=15;
-	arrint[3]=16;
-	arrint[4]=23;
-	arrint[5]=42;
-	
-	//NUMERO DE ELEMENTOS CONOCIDO
-	numelem=6; //Tampoco se de donde vas a sacar esto... haha
+	input[0]=4;
+	input[1]=8;
+	input[2]=15;
+	input[3]=16;
+	input[4]=23;
+	input[5]=42;
 	
 	printf("State of art:\n");
-	printf("int: %d, short: %d, char: %d\n", sizeof(int), sizeof(short), sizeof(char));
+	printf("int: %ld, short: %ld, char: %ld\n", sizeof(int), sizeof(short), sizeof(char));
 
 	for(i=0;i<numelem;i++){
-		printf("\narrint[%d] = %d",i,arrint[i]);
-		sprintf(arrcha[i],"%d",arrint[i]);
-		printf("\narrcha[%d] = %s \n",i,arrcha[i]);
+		printf("\ninput[%d] = %d",i,input[i]);
+		sprintf(output[i],"%d",input[i]);
+		printf("\noutput[%d] = %s \n",i,output[i]);
 	}
 	
 	return 0;	
