4b9d78e46b17083467bfd0193fac44176b36a3c3
Dummy Hoe <dvdvck@gmail.com>
2014-03-20 18:17:50 -0600

    Cambiando la forma de establecer el numero de input
    A traves de la linea de comando se especfica cuantos elementos tiene
    el array en cuestion.

--- scanning.c
+++ scanning.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main ( void ){
 
-	int arrint[256];
-	char arrcha[256][12];
-	int i;
-	int numelem=0;
+int main (int argv, char *argv[]){
+
+	unsigned short *input;
+	unsigned char *output;
+	int numelem;
+
+	numelem = strtol(argv[1], NULL, 0);
+
+	if(numelem == 0){
+		numelem = 6;
+	}
 	
+
 	//Llenado manual... no se de donde vayan a salir estos datos
 	arrint[0]=4;
 	arrint[1]=8;
@@ -14,9 +22,6 @@ int main ( void ){
 	arrint[3]=16;
 	arrint[4]=23;
 	arrint[5]=42;
-	
-	//NUMERO DE ELEMENTOS CONOCIDO
-	numelem=6; //Tampoco se de donde vas a sacar esto... haha
 	
 	printf("State of art:\n");
 	printf("int: %d, short: %d, char: %d\n", sizeof(int), sizeof(short), sizeof(char));
